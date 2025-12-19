c32544903cec3f151f00d07f2ea486c93428ccba
bhavani muddala <bhavanimuddala10@gmail.com>
2025-12-19 11:30:13 -0500

    two way data binding

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,7 @@ import { TemplateRefDemo } from './template-ref-demo/template-ref-demo';
 import { StructuralDirectivesDemo } from './structural-directives-demo/structural-directives-demo';
 import { NgTemplateOutletDemo } from './ng-template-outlet-demo/ng-template-outlet-demo';
 import { ProductListComponent } from './product-list/product-list';
+import { TwoWayBinding } from './two-way-binding/two-way-binding';
 export const routes: Routes = [
     { path: 'home', component: Home },
     { path: 'interpolation', component: InterpolationComponent },
@@ -17,5 +18,6 @@ export const routes: Routes = [
     { path: 'TemplateRef', component: TemplateRefDemo },
     {path:'StructuralDirectives',component:StructuralDirectivesDemo},
     {path:'NgTemplateOutlet',component:NgTemplateOutletDemo},
-    {path:'ProductListComponent',component:ProductListComponent}
+    {path:'ProductListComponent',component:ProductListComponent},
+    {path:"TwoWayBinding",component:TwoWayBinding}
 ];
